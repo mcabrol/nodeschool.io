e48f000981810e4603876166224eb5d0283a93c5
Martin Cabrol <mcabrol@e-r7-p2.s19.be>
2019-04-04 17:02:54 +0200

    Erase *.txt

--- Learnyounode/make_it_modular.js
+++ Learnyounode/make_it_modular.js
@@ -3,6 +3,6 @@ var module = require('./mymodule.js');
 var dir = process.argv[2];
 var chr = process.argv[3];
 
-var filtered =  module(dir, chr);
+var filtered = module(dir, chr);
 
 console.log(filtered);
